571dd76f26ad0428c70bb306c9b3f9696f1c7970
ubk8751 <ubk8751@gmail.com>
2023-04-14 14:29:37 +0200

    init_commit. No actual work has been done

---
+++ ass1/AIs.py
@@ -0,0 +1,28 @@
+# Importing the libs to work
+# Maths
+import numpy as np
+import matplotlib as plt
+
+# Models
+# ResNet-50
+from keras.applications.resnet import ResNet50
+from keras.applications.resnet import preprocess_input
+
+
+# Densenet-121
+from keras.applications.densenet import DenseNet121
+from keras.applications.densenet import preprocess_input
+
+# VGG-19
+from keras.applications.vgg19 import VGG19
+from keras.applications.vgg19 import preprocess_input
+
+# Other
+from keras.optimizers import Adam, SGD
+from keras.models import Model, load_model
+from keras.layers import *
+from sklearn.model_selection import train_test_split
+from keras.callbacks import *
+from keras.applications.resnet import decode_predictions
+from keras.preprocessing.image import image
+from keras.preprocessing.image import img_to_array
---
+++ ass1/GradDescent.py
@@ -0,0 +1,15 @@
+
+
+def gradDecent(x,y, n, lr, lst):
+    
+    return lst
+    
+
+if __name__ == "__main__":
+    startValX = 1
+    startValY = 1
+    n = 20
+    lr = 0.01
+    resList = []
+
+    gradDecent(startValX, startValY, n, lr, resList)
