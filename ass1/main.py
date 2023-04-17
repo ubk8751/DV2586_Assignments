266c7f743cd852e8fd2c6f5111cb5af76c4e794c
ubk8751 <ubk8751@gmail.com>
2023-04-17 16:19:47 +0200

    dida

--- ass1/main.py
+++ ass1/main.py
@@ -1,6 +1,6 @@
-from AI import gen_models, gen_imgGPT, gen_densenet, gen_vgg, gen_resnet
+from AI import get_models, get_imgGPT, get_densenet, get_vgg, get_resnet
 
 
 if __name__ == "__main__":
-    #vgg, densenet, resnet, imgGPT = gen_models()
+    #vgg, densenet, resnet, imgGPT = get_models()
     pass
